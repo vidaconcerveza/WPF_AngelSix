b3c01915eb9c0245a7217d838dc3773e8e780a23
vidaconcerveza <vidaconcerveza@hotmail.com>
2020-01-14 09:17:12 +0900

    Custom window chrome and styles

--- wpfBasics/Fasetto.Word/ViewModel/WindowViewModel.cs
+++ wpfBasics/Fasetto.Word/ViewModel/WindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Fasetto.Word
 {
@@ -24,6 +27,15 @@ namespace Fasetto.Word
         #region Public Properties
 
         /// <summary>
+        /// The smallest width and Height of the window
+        /// </summary>
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 400;
+
+
+
+
+        /// <summary>
         /// The size of the resize border around the window
         /// </summary>
         public int ResizeBorder { get; set; } = 6;
@@ -32,7 +44,13 @@ namespace Fasetto.Word
         /// The Size of the resize border around the windows, taking into account the outer margin
         /// </summary>
         public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder+OuterMarginSize); } }
-        
+
+        /// <summary>
+        /// The padding of the inner content of the main window
+        /// </summary>
+        public Thickness InnerContentPadding { get { return new Thickness(ResizeBorder); } }
+
+
         public int OuterMarginSize
         {
             get
@@ -76,6 +94,29 @@ namespace Fasetto.Word
         public int TitleHeight { get; set; } = 42;
 
         public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight+ResizeBorder); } }
+
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// To Command to minimize the window
+        /// </summary>
+        public ICommand MinimizedCommand { get; set; }
+
+        /// <summary>
+        /// To Command to Maximize the window
+        /// </summary>
+        public ICommand MaximizedCommand { get; set; }
+
+        /// <summary>
+        /// To Command to Close the window
+        /// </summary>
+        public ICommand CloseCommand { get; set; }
+
+        /// <summary>
+        /// To Command to show menu
+        /// </summary>
+        public ICommand MenuCommand { get; set; }
         #endregion
 
 
@@ -94,9 +135,41 @@ namespace Fasetto.Word
                  OnPropertyChanged(nameof(WindowRadius));
                  OnPropertyChanged(nameof(WindowCornerRadius));
              };
+
+            //Create Commands
+            MinimizedCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
+            MaximizedCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
+            // Fix Window resize issue
+            var resizer = new WindowResizer(mWindow);
+
         }
         #endregion
 
+        #region Private Helpers
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 x;
+            public Int32 y;
+        }
+        /// <summary>
+        /// Gets te current mouse position on the screen.
+        /// </summary>
+        /// <returns></returns>
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.x, w32Mouse.y);
+        }
+
+        #endregion
 
     }
 }
